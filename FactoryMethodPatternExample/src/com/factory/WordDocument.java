28081146353e91067c189dc53d148b7843aeac91
Gopi Madhavi <gopimadhavi07@gmail.com>
2026-06-21 23:47:53 +0530

    Removed duplicate root folders

--- FactoryMethodPatternExample/src/com/factory/WordDocument.java
+++
@@ -1,9 +0,0 @@
-package com.factory;
-
-public class WordDocument implements Document {
-
-    @Override
-    public void open() {
-        System.out.println("Word Document Opened");
-    }
-}
--- FactoryMethodPatternExample/src/com/factory/WordFactory.java
+++
@@ -1,9 +0,0 @@
-package com.factory;
-
-public class WordFactory extends DocumentFactory {
-
-    @Override
-    public Document createDocument() {
-        return new WordDocument();
-    }
-}
