b720815c848bffc1aad82fb9cbb873c2841798f7
Gopi Madhavi <gopimadhavi07@gmail.com>
2026-06-26 13:40:08 +0530

    Added Exercise 4 - AAA Pattern, Setup and Teardown in JUnit

---
+++ Week1/AAAPatternJUnit/src/com/example/CalculatorTest.java
@@ -0,0 +1,39 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+        System.out.println("Setup executed");
+    }
+
+    @After
+    public void tearDown() {
+        calculator = null;
+        System.out.println("Teardown executed");
+    }
+
+    @Test
+    public void testAddition() {
+
+        // Arrange
+        int a = 10;
+        int b = 20;
+
+        // Act
+        int result = calculator.add(a, b);
+
+        // Assert
+        assertEquals(30, result);
+
+    }
+}
